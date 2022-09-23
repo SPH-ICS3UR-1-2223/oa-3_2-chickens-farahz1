29bcda7d9a9825941e8b103e7a3e7f734172291d
farahz1 <farah.al-jamal@stu.ocsb.ca>
2022-09-23 14:39:11 -0400

    chicken1&2

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,18 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
+        int totalEggs=0;
+        int eggsPerChicken=5;
+        int chickencount=3;
+        //Monday
+        totalEggs = eggsPerChicken * chickencount;
+       //Tuesday
+        chickencount++;
+        totalEggs += eggsPerChicken * chickencount;
+        //Wednesday
+        		
+        chickencount=
         
-        System.out.println(totalEggs);
+        System.out.println("totalEggs");
     }   
 }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,9 +1,25 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
+        int dailyAverage =0;
+        double monthlyAverage=0;
+        int eggCount =0;
+        double monthlyProfit =0;
+        		//Monday
+        		eggCount+=100; //Collect 100 eggs 
+        //Tuesday 
+        eggCount+=121; //collect 121 eggs 
+        //Wednesday 
+        eggCount += 117; // collect 117 eggs
+        //Calculate daily Average
+        
+        dailyAverage = eggCount/3;
         System.out.println("Daily Average:   " +dailyAverage);
+        //Calculate monthly average
+        monthlyAverage = dailyAverage*30;
         System.out.println("Monthly Average: " +monthlyAverage);
+        //calculate monthly profit 
+        monthlyProfit = monthlyAverage*0.18; 
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
     
